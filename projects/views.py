e4e1e1885b55a63950a18614abab80bb57fda327
LukeCallahan <lukemcal@gmail.com>
2022-11-09 16:22:37 -0800

    wip lesson 7 rendering data to templates video

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+    {
+    'id': '1',
+    'title': "Ecommerce Website",
+    'description': 'Fully functional ecommerce website'
+    },
+    {
+    'id': '2',
+    'title': "Portfolio Website",
+    'description': 'This is a project where I built out my portfolio'
+    },
+    {
+    'id': '1',
+    'title': "Social Network",
+    'description': 'Awesome open source project I am still working on.'
+    },
+]
+
 def projects(request):
-    return render(request, 'projects/projects.html')
+    page = 'projects'
+    number = 10
+    context = {'page': page, 'number': number}
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
     return render(request, 'projects/single-project.html')
